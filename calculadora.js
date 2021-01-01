5ab0109455d8b3f5a161cf408e052ce76dd41d42
thiagodevx <thiago.dev.x@gmail.com>
2021-01-01 17:42:07 -0300

    inicio do curso, com os fundamentos de nodejs

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+const soma = (a, b) => a + b;
+const subtracao = (a, b) => a - b;
+const divisao = (a, b) => a / b;
+const multiplicacao = (a, b) => a * b;
+
+class Calculadora {
+  soma;
+  subtracao;
+  divisao;
+  multiplicacao;
+}
